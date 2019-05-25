5d7a308667f7ee51a1482e637e21e45849e1d559
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2019-05-25 11:09:28 -0500

    remove useless comments

--- fork/src/main/java/org/apache/tika/fork/TikaProcessPool.java
+++ fork/src/main/java/org/apache/tika/fork/TikaProcessPool.java
@@ -32,13 +32,12 @@ public class TikaProcessPool implements AutoCloseable {
       try {
         return process.parse(contentInputStream, contentOutputStream);
       } catch (Exception e) {
-        // invalidate the object
         pool.invalidateObject(process);
-        // do not return the object to the pool twice
+        // Do not return the object to the pool twice
         process = null;
         throw new RuntimeException("Could not parse content input stream", e);
       } finally {
-        // make sure the object is returned to the pool
+        // Make sure the object is returned to the pool
         if (null != process) {
           pool.returnObject(process);
         }
@@ -48,21 +47,15 @@ public class TikaProcessPool implements AutoCloseable {
     }
   }
 
-  //A helper method to initialize the pool using the config and object-factory.
   public static ObjectPool initializePool(int numMinIdle, int numMaxIdle, int numMaxTotal, String tikaDistDir) throws Exception {
-    // We configure the pool using a GenericObjectPoolConfig
-    //Note: In the default implementation of Object Pool, objects are not created at start-up, but rather are created whenever the first call
-    //to the pool.borrowObject() is made. This object is then cached for future use.
-    //It is recommended to put these settings in a properties file.
+    // Note: In the default implementation of Object Pool, objects are not created at start-up, but rather are created whenever the first call
+    // to the pool.borrowObject() is made. This object is then cached for future use.
     GenericObjectPoolConfig config = new GenericObjectPoolConfig();
     config.setMinIdle(numMinIdle);
     config.setMaxIdle(numMaxIdle);
     config.setMaxTotal(numMaxTotal);
     config.setBlockWhenExhausted(true);
 
-    //We use the GenericObjectPool implementation of Object Pool as this suffices for most needs.
-    //When we create the object pool, we need to pass the Object Factory class that would be responsible for creating the objects.
-    //Also pass the config to the pool while creation.
     ObjectPool pool = new GenericObjectPool<TikaProcess>(new TikaProcessFactory(tikaDistDir), config);
 
     return pool;
